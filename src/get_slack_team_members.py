9239f835acc1ee47a12cf7f91e37739982a6dc2e
Sarah Gibson <drsarahlgibson@gmail.com>
2022-05-03 18:42:02 +0100

    Tidy up get_slack_team_members.py

--- src/get_slack_team_members.py
+++ src/get_slack_team_members.py
@@ -1,19 +1,12 @@
 import os
-from pathlib import Path
 
-from dotenv import load_dotenv
 from slack_sdk import WebClient
 
 
 class SlackTeamMembers:
     def __init__(self):
-        # Load variables from a .env file
-        project_path = Path(__file__).parent.parent
-        dotenv_path = project_path.joinpath(".env")
-        load_dotenv(dotenv_path=dotenv_path)
-
         # Set variables
-        self.team_name = os.getenv("TEAM_NAME")
+        self.team_name = os.environ["TEAM_NAME"]
 
         # Instantiate a SLACK API client
         self.client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
@@ -24,16 +17,17 @@ class SlackTeamMembers:
             api_method="usergroups.list",
         )
 
-        # Find IDs for the self.team_name usergroup
+        # Find ID for the self.team_name usergroup
         index = next(
             idx
             for (idx, usergroup) in enumerate(response["usergroups"])
             if usergroup["handle"] == self.team_name
         )
         self.usergroup_id = response["usergroups"][index]["id"]
-        self.team_id = response["usergroups"][index]["team_id"]
 
     def _get_user_ids(self):
+        self._get_team_id()
+
         # Find all user IDs in the self.team_name usergroup
         response = self.client.api_call(
             api_method="usergroups.users.list",
@@ -55,16 +49,18 @@ class SlackTeamMembers:
         return username
 
     def get_users_in_team(self):
-        self._get_team_id()
         self._get_user_ids()
 
         user_handles = []
         for user_id in self.user_ids:
             user_handles.append(self._convert_user_id_to_handle(user_id))
 
-        return user_handles
+        return sorted(user_handles)
 
 
 if __name__ == "__main__":
+    from rich import print_json
+
     app = SlackTeamMembers()
     usernames = app.get_users_in_team()
+    print_json(data=usernames)
