25f76fd8f53f6a8507d0f5c0a41fa7fabd220002
Sarah Gibson <drsarahlgibson@gmail.com>
2022-05-05 10:35:40 +0100

    Write script to initialise team-roles.json with current serving members

---
+++ src/set_current_roles.py
@@ -0,0 +1,44 @@
+"""
+Function to populate team-roles.json with the current team members serving in our Team Roles
+"""
+import json
+import os
+from pathlib import Path
+
+from .get_slack_team_members import SlackTeamMembers
+
+
+def main():
+    # Set paths
+    project_path = Path(__file__).parent.parent
+    roles_path = project_path.joinpath("team-roles.json")
+
+    # Set environment variables
+    current_meeting_facilitator = os.environ["CURRENT_MEETING_FACILITATOR"]
+    current_support_steward = os.environ["CURRENT_SUPPORT_STEWARD"]
+    incoming_support_steward = os.environ["INCOMING_SUPPORT_STEWARD"]
+
+    # Instantiate SlackTeamMembers class
+    slack = SlackTeamMembers()
+    members = slack.get_users_in_team()
+
+    # Write team roles dict
+    team_roles = {
+        "meeting_facilitator": {
+            "name": current_meeting_facilitator,
+            "id": members[current_meeting_facilitator]
+        },
+        "support_steward": {
+            "incoming": {
+                "name": incoming_support_steward,
+                "id": members[incoming_support_steward]
+            },
+            "current": {
+                "name": current_support_steward,
+                "id": members[current_support_steward]
+            }
+        }
+    }
+
+    with open(roles_path, "w") as f:
+        json.dump(team_roles, f)
