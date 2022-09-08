a6ca546928576352702329a4f8a45e40885af3f5
Sarah Gibson <drsarahlgibson@gmail.com>
2022-09-08 18:56:57 +0100

    Refactor create_events_bulk to use events handler

--- src/calendar/create_events_bulk.py
+++ src/calendar/create_events_bulk.py
@@ -5,208 +5,165 @@ import argparse
 import json
 import os
 from datetime import datetime, timedelta
-from itertools import cycle, islice
 from pathlib import Path
 
-from dateutil.relativedelta import relativedelta
-from googleapiclient.errors import HttpError
 from loguru import logger
 from rich.progress import track
 from rich.prompt import Confirm
 
-from ..encryption.sops import get_decrypted_file
-from ..geekbot.get_slack_usergroup_members import SlackUsergroupMembers
+from .event_handling import ROLE_CYCLES, CalendarEventHandler
 
 
-class CreateBulkEvents:
-    """Create Team Role events in a Calendar in Bulk"""
+class MutuallyInclusiveArgumentError(Exception):
+    pass
 
-    def __init__(self, date=None):
-        self._generate_reference_date(date=date)
-        usergroup_name = os.environ["USERGROUP_NAME"]
-        self.usergroup_members = (
-            SlackUsergroupMembers().get_users_in_usergroup(usergroup_name).keys()
-        )
 
-        # Set filepaths
-        project_path = Path(__file__).parent.parent.parent
-        roles_path = project_path.joinpath("team-roles.json")
-   
-        # Check the team roles file exists before reading
-        if not os.path.exists(roles_path):
-            raise FileNotFoundError(f"File must exist to continue! {roles_path}")
+def adjust_reference_date(reference_date):
+    """The Support Steward Role is transferred on Wednesdays. We adjust the reference
+    date to be the next Wednesday from the given date for the calculations.
 
-        with open(roles_path) as stream:
-            self.team_roles = json.load(stream)
+    Args:
+        reference_date (date obj): The date to be adjusted
 
-    def _generate_reference_date(self, date=None):
-        """Generate a reference date to calculate start and end dates for role events
-        from. Defaults to the on which the program is run.
+    Returns:
+        reference_date (date obj): The adjusted date
+    """
+    # isoweekday() returns an integer representation of the day of the week where
+    # MONDAY is 1 and SUNDAY is 7. Hence, WEDNESDAY is 3.
+    weekday_num = reference_date.isoweekday()
 
-        Args:
-            date (str, optional): A chosen reference date as a string in the format
-                'YYYY-MM-DD'. Defaults to None.
-        """
-        if date is None:
-            self.reference_date = datetime.today()
-        else:
-            self.reference_date = datetime.strptime(date, "%Y-%m-%d")
+    if weekday_num < 3:
+        reference_date = reference_date + timedelta(days=(3 - weekday_num))
+    elif weekday_num > 3:
+        reference_date = reference_date + timedelta(days=(7 + (3 - weekday_num)))
 
-        logger.info(
-            "Reference date to calculate events from: {}",
-            self.reference_date.strftime("%Y-%m-D"),
-        )
+    logger.info(
+        "Adjusting reference date for Support Steward role to: {}",
+        reference_date.strftime("%Y-%m-%d"),
+    )
 
-    def _adjust_reference_date(self):
-        """
-        The Support Steward Role is transferred on Wednesdays. We adjust the reference
-        date to be the next Wednesday from the given date for the calculations.
-        """
-        # isoweekday() returns an integer representation of the day of the week where
-        # MONDAY is 1 and SUNDAY is 7. Hence, WEDNESDAY is 3.
-        weekday_num = self.reference_date.isoweekday()
+    return reference_date
 
-        if weekday_num < 3:
-            self.reference_date = self.reference_date + timedelta(
-                days=(3 - weekday_num)
-            )
-        elif weekday_num > 3:
-            self.reference_date = self.reference_date + timedelta(
-                days=(7 + (3 - weekday_num))
-            )
 
-        logger.info(
-            "Reference date adjusted to: {}", self.reference_date.strftime("%Y-%m-%d")
-        )
+def read_team_roles_from_file(role):
+    """If a team member cannot be read from a calendar event, and one hasn't been
+    provided via the command line, we fall back onto reading the team member from
+    the team-roles.json file.
 
-    def _generate_event_metadata(self, role, name, offset):
-        """Generate metadata for an event to be created in a Google Calendar
+    Args:
+        role (str): The role we are interested in learning about. Can be either
+            'meeting-facilitator' or 'support-steward'.
 
-        Args:
-            role (str): The role to create an event for. Either 'meeting-facilitator'
-                or 'support-steward'.
-            name (str): The name of the team member who will serve in this role for
-                this event
-            offset (int): The offset from the reference date. The units of this value
-                is described by ROLE_CYCLES[role]["units"].
+    Raises:
+        FileNotFoundError: If the file does not exist at the expected location,
+            immediately exit the script
 
-        Returns:
-            dict: A dictionary of calendar event metadata. Items include the start and
-                end times of the event, and a summary.
-        """
-        if role == "meeting-facilitator":
-            start_date, end_date = self._calculate_event_dates_meeting_facilitator(
-                offset
-            )
-        elif role == "support-steward":
-            start_date, end_date = self._calculate_event_dates_support_steward(offset)
+    Returns:
+        str: The team member currently serving in the specified role
+    """
+    # Set filepaths
+    project_path = Path(__file__).parent.parent.parent
+    roles_path = project_path.joinpath("team-roles.json")
 
-        # This represents the minimum amount of information to POST to the Google
-        # Calendar API to create an event in a given calendar.
-        body = {
-            "summary": f"{' '.join(role.split('-')).title()}: {name.split()[0]}",
-            "start": {
-                "date": start_date.strftime("%Y-%m-%d"),
-                "timeZone": "Etc/UTC",
-            },
-            "end": {
-                "date": end_date.strftime("%Y-%m-%d"),
-                "timeZone": "Etc/UTC",
-            },
-        }
+    # Check the team roles file exists before reading
+    if not os.path.exists(roles_path):
+        raise FileNotFoundError(f"File must exist to continue! {roles_path}")
 
-        return body
+    with open(roles_path) as stream:
+        team_roles = json.load(stream)
 
-    def _create_event(self, event_info):
-        """Create an event in a Google Calendar
+    if role == "meeting-facilitator":
+        member = team_roles[role.replace("-", "_")]["name"]
+    elif role == "support-steward":
+        member = team_roles[role.replace("-", "_")]["current"]["name"]
 
-        Args:
-            event_info (dict): Metadata describing the event to create. Must include
-                start and end dates, and a summary.
-        """
-        try:
-            # Create the event
-            self.gcal_api.events().insert(
-                calendarId=self.calendar_id, body=event_info
-            ).execute()
+    return member
 
-        except HttpError as error:
-            logger.error(f"An error occured: {error}")
 
-    def create_bulk_events(self, role, name=None, n_events=None):
-        """Bulk create Team Role events in a Google Calendar
+def create_bulk_events(role, n_events=None, ref_date=None, member=None):
+    """Create role events in bulk in the Team Roles Calendar
 
-        Args:
-            role (str): The role to create events for. Either 'meeting-facilitator' or
-                'support-steward'.
-            name (str, optional): The name of the current team member serving in the
-                role. Defaults to None, and will be pulled from team-roles.json.
-            n_events (int, optional): The number of events to create for the specified
-                role. Defaults to ROLE_CYCLES[role]["n_events"].
-        """
-        # Find the name of the person currently serving in this role from
-        # team-roles.json or use a provided 'name' variable
-        if role == "meeting-facilitator":
-            current_member = (
-                self.team_roles[role.replace("-", "_")]["name"]
-                if name is None
-                else name
-            )
-        elif role == "support-steward":
-            self._adjust_reference_date()
-            current_member = (
-                self.team_roles[role.replace("-", "_")]["current"]["name"]
-                if name is None
-                else name
-            )
+    Args:
+        role (str): The role we want to create events for
+        n_events (int, optional): The number of events to create. Defaults to None
+            and extracts a set value from the ROLE_CYCLES dictionary.
+        ref_date (date obj, optional): A reference date to begin creating events from.
+            Defaults to None.
+        member (str, optional): The team member currently serving in the specified role.
+            Defaults to None.
+    """
+    # Set variables from the environment
+    usergroup_name = os.environ["USERGROUP_NAME"]
 
-        # Find the index of the current team member in the ordered list
-        current_member_index = next(
-            (
-                i
-                for (i, name) in enumerate(self.usergroup_members)
-                if current_member.lower() in name.lower()
-            ),
-            None,
-        )
+    # Set the number of events to create if not specified
+    if n_events is None:
+        n_events = ROLE_CYCLES[role]["n_events"]
 
-        # Set the number of events to create if not specified
-        if n_events is None:
-            n_events = ROLE_CYCLES[role]["n_events"]
+    if ref_date is not None:
+        ref_date = datetime.strptime(ref_date, "%Y-%m-%d")
+        if role == "support-steward":
+            ref_date = adjust_reference_date(ref_date)
 
-        # Create a repeating list of team members (in order) long enough to index for
-        # the number of events we will create
-        members = list(
-            islice(
-                cycle(self.usergroup_members),
-                current_member_index + 1,
-                n_events + current_member_index + 1,
-            )
-        )
+    # Instantiate the event handler
+    event_handler = CalendarEventHandler(role, usergroup_name)
 
-        logger.info("Generating upcoming events...")
+    logger.info("Generating new event metadata...")
 
-        # Generate the events
+    # Get existing events from the calendar
+    ext_events = []  # event_handler._get_upcoming_events()
+    if ext_events:
         events = []
         for i in range(n_events):
-            next_member = members[i]
-            event = self._generate_event_metadata(role, next_member, i)
-            print(
-                event["start"]["date"],
-                "->",
-                event["end"]["date"],
-                ":",
-                event["summary"],
+            events.append(
+                event_handler.calculate_next_event_data(offset=i, suppress_logs=True)
             )
-            events.append(event)
+    else:
+        if member is None:
+            # Read in team-roles.json file
+            logger.warning(
+                "There are no previous events found in the calendar and a team member has not been provided. "
+                "I will pull the name of the current team member from the team-roles.json file. "
+                "Double check the generated events before creating them! "
+                "If you don't want to create the suggested events, exit the script and rerun setting the --date [-d] and --team-member [-m] flags."
+            )
+            member = read_team_roles_from_file(role)
 
-        confirm = Confirm.ask("Create these events?", default=False)
+            logger.warning(f"Current team member set to: {member}")
 
-        if confirm:
-            for event in track(events, description="Creating calendar events..."):
-                self._create_event(event)
-        else:
-            logger.info("Ok! Exiting with out creating any events")
+        if ref_date is None:
+            ref_date = datetime.today()
+
+            if role == "meeting-facilitator":
+                ref_date = ref_date.replace(month=ref_date.month + 1)
+                ref_date = ref_date.replace(day=1)
+            elif role == "support-steward":
+                ref_date = adjust_reference_date(ref_date)
+
+            logger.warning(
+                "There are no previous events found in the calendar and a reference date has not been provided. "
+                f"I will set a reference date of {ref_date.strftime('%Y-%m-%d')}. "
+                "Double check the generated events before creating them! "
+                "If you don't want to create the suggested events, exit the script and rerun setting the --date [-d] and --team-member [-m] flags."
+            )
+
+        events = []
+        for i in range(n_events):
+            events.append(
+                event_handler.calculate_next_event_data(
+                    ref_date=ref_date, member=member, offset=i, suppress_logs=True
+                )
+            )
+
+    for event in events:
+        print(f"{event['start']['date']} -> {event['end']['date']}: {event['summary']}")
+
+    confirm = Confirm.ask("Create these events?", default=False)
+
+    if confirm:
+        for event in track(events, description="Creating calendar events..."):
+            event_handler.create_event(event)
+    else:
+        logger.info("Ok! Exiting with out creating any events")
 
 
 def main():
@@ -220,31 +177,53 @@ def main():
         help="The role to create events for",
     )
     parser.add_argument(
-        "-m",
-        "--team-member",
-        type=str,
-        default=None,
-        help="The name of the team member currently serving in the role. Will be pulled from team-roles.json if not provided.",
-    )
-    parser.add_argument(
         "-n",
         "--n-events",
         type=int,
         default=None,
-        help="The number of role events to create. Defaults to 12 for Meeting Facilitator and 26 for Support Steward (both 1 year's worth).",
+        help=(
+            "The number of role events to create. "
+            "Defaults to 12 for Meeting Facilitator and 26 for Support Steward (both 1 year's worth)."
+        ),
     )
     parser.add_argument(
         "-d",
         "--date",
         type=str,
         default=None,
-        help="A reference date to begin creating events from. Defaults to today. WARNING: EXPERIMENTAL FEATURE.",
+        help=(
+            "A reference date to begin creating events from. "
+            "Defaults to appending events from the last in the series, or TODAY if no events exist. "
+            "WARNING: EXPERIMENTAL FEATURE. "
+            "This flag is MUTUALLY INCLUSIVE with --team-member [-m]."
+        ),
+    )
+    parser.add_argument(
+        "-m",
+        "--team-member",
+        type=str,
+        default=None,
+        help=(
+            "The name of the team member currently serving in the role. "
+            "Defaults to being pulled from either the last calendar event, or team-roles.json if a calendar event doesn't not exist. "
+            "This flag is MUTUALLY INCLUSIVE with --date [-d]."
+        ),
     )
 
     args = parser.parse_args()
 
-    CreateBulkEvents(date=args.date).create_bulk_events(
-        args.role, name=args.team_member, n_events=args.n_events
+    if (args.date is not None and args.team_member is None) or (
+        args.date is None and args.team_member is not None
+    ):
+        raise MutuallyInclusiveArgumentError(
+            "Both --team-member and --date options must be provided"
+        )
+
+    create_bulk_events(
+        args.role,
+        n_events=args.n_events,
+        ref_date=args.date,
+        member=args.team_member,
     )
 
 
