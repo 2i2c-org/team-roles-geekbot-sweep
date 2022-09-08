ce1347e03ec95ebb1fcaf566fc6f470e8c23c60c
Sarah Gibson <drsarahlgibson@gmail.com>
2022-09-08 11:54:15 +0100

    Move ROLE_CYCLES dict into centralised loc

--- src/calendar/create_events_bulk.py
+++ src/calendar/create_events_bulk.py
@@ -18,22 +18,6 @@ from ..encryption.sops import get_decrypted_file
 from ..geekbot.get_slack_usergroup_members import SlackUsergroupMembers
 from .gcal_api_auth import GoogleCalendarAPI
 
-# Some information about how often each of our team roles is transferred
-ROLE_CYCLES = {
-    "meeting-facilitator": {
-        "unit": "months",
-        "frequency": 1,  # Monthly
-        "period": 1,
-        "n_events": 12,  # Equates to 1 year
-    },
-    "support-steward": {
-        "unit": "days",
-        "frequency": 14,  # Fortnightly
-        "period": 28,  # 4 weeks
-        "n_events": 26,  # Equates to 1 year
-    },
-}
-
 
 class CreateBulkEvents:
     """Create Team Role events in a Calendar in Bulk"""
---
+++ src/calendar/event_handling.py
@@ -0,0 +1,20 @@
+# Some information about how often each of our team roles is transferred
+ROLE_CYCLES = {
+    "meeting-facilitator": {
+        "unit": "months",
+        "frequency": 1,  # Monthly
+        "period": 1,
+        "n_events": 12,  # Equates to 1 year
+    },
+    "support-steward": {
+        "unit": "days",
+        "frequency": 14,  # Fortnightly
+        "period": 28,  # 4 weeks
+        "n_events": 26,  # Equates to 1 year
+    },
+}
+
+
+class CalendarEventHandler:
+    def __init__(self):
+        pass
