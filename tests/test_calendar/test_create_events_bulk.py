d7ad6516ce11af5d405b5de9edce421789af915e
Sarah Gibson <drsarahlgibson@gmail.com>
2022-09-18 11:38:22 -0700

    Test some funcs in create_events_bulk.py

---
+++ tests/test_calendar/test_create_events_bulk.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from pathlib import Path
+from unittest.mock import patch
+
+from src.calendar.create_events_bulk import (
+    adjust_reference_date,
+    read_team_roles_from_file,
+)
+
+
+def test_adjust_reference_date_less_than_three():
+    test_date = datetime(2022, 9, 19)
+    expected_date = datetime(2022, 9, 21)
+    result_date = adjust_reference_date(test_date)
+
+    assert result_date == expected_date
+
+
+def test_adjust_reference_date_more_than_three():
+    test_date = datetime(2022, 9, 23)
+    expected_date = datetime(2022, 9, 28)
+    result_date = adjust_reference_date(test_date)
+
+    assert result_date == expected_date
